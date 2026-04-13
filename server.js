0dab61e9d867a2759563f7064d8785a447e28722
TheDuyet <32725226+duyetnt90@users.noreply.github.com>
2026-04-13 22:24:58 +0700

    Update server.js

--- server.js
+++ server.js
@@ -59,6 +59,11 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-server.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
+//server.listen(process.env.PORT, () => {
+//    console.log(`Server running on port ${process.env.PORT}`);
+//});
