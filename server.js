c4c570f1aa5cd664f9212423dbc091e7454528c2
DuyetNT <duyetnt.dev@gmail.com>
2026-04-13 23:11:16 +0700

    update port

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const io = new Server(server, {
 
 // middleware
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true
 }));
@@ -60,7 +60,7 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, '0.0.0.0', () => {
+server.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
 
